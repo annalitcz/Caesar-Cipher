07ce68c45630881ff3a04c9c8e12ac3236db0d6a
Anzz <makrufmustofa79@gmail.com>
2023-04-25 18:42:16 +0700

    Update

--- index.php
+++ index.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <title>Caesar Cipher</title>
 </head>
 <style>
-  .container{
+  .container {
     display: flex;
     justify-content: space-evenly;
     align-items: center;
     margin-top: 50px;
   }
-  h1{
+
+  h1 {
     text-align: center;
   }
-  p{
+
+  p {
     text-align: center;
   }
 </style>
+
 <body>
   <h1>Caesar Cipher</h1>
   <div class="container">
@@ -40,60 +44,65 @@
     </form>
   </div>
   <?php
-    function encrypt($plaintext, $key) {
-      $ciphertext = "";
-      $length = strlen($plaintext);
-      for ($i = 0; $i < $length; $i++) {
-        $char = ord($plaintext[$i]);
-        if ($char >= 65 && $char <= 90) {
-          // uppercase letters
-          $newChar = (($char - 65 + $key) % 26) + 65;
-        } else if ($char >= 97 && $char <= 122) {
-          // lowercase letters
-          $newChar = (($char - 97 + $key) % 26) + 97;
-        } else {
-          // non-alphabetic characters
-          $newChar = $char;
-        }
-        $ciphertext .= chr($newChar);
+
+  if (isset($_POST['encrypt']) && isset($_POST['plaintext']) && isset($_POST['key'])) {
+    $plaintext = $_POST['plaintext'];
+    $key = $_POST['key'];
+    $ciphertext = encrypt($plaintext, $key);
+    echo "<p>Plain Text : " . $plaintext . "</p>";
+    echo "<p>Key : " . $key . "</p>";
+    echo "<p>Encrypted Text: " . $ciphertext . "</p>";
+  }
+  if (isset($_POST['decrypt']) && isset($_POST['cipher-text']) && isset($_POST['key'])) {
+    $ciphertext = $_POST['cipher-text'];
+    $key = $_POST['key'];
+    $plaintext = decrypt($ciphertext, $key);
+    echo "<p>Cipher Text : " . $ciphertext . "</p>";
+    echo "<p>Key : " . $key . "</p>";
+    echo "<p>Decrypted Text : " . $plaintext . "</p>";
+  }
+  function encrypt($plaintext, $key)
+  {
+    $ciphertext = "";
+    $length = strlen($plaintext);
+    for ($i = 0; $i < $length; $i++) {
+      $char = ord($plaintext[$i]);
+      if ($char >= 65 && $char <= 90) {
+        // uppercase letters
+        $newChar = (($char - 65 + $key) % 26) + 65;
+      } else if ($char >= 97 && $char <= 122) {
+        // lowercase letters
+        $newChar = (($char - 97 + $key) % 26) + 97;
+      } else {
+        // non-alphabetic characters
+        $newChar = $char;
       }
-      return $ciphertext;
+      $ciphertext .= chr($newChar);
     }
-    
-    function decrypt($ciphertext, $key) {
-      $plaintext = "";
-      $length = strlen($ciphertext);
-      for ($i = 0; $i < $length; $i++) {
-        $char = ord($ciphertext[$i]);
-        if ($char >= 65 && $char <= 90) {
-          // uppercase letters
-          $newChar = (($char - 65 - $key + 26) % 26) + 65;
-        } else if ($char >= 97 && $char <= 122) {
-          // lowercase letters
-          $newChar = (($char - 97 - $key + 26) % 26) + 97;
-        } else {
-          // non-alphabetic characters
-          $newChar = $char;
-        }
-        $plaintext .= chr($newChar);
+    return $ciphertext;
+  }
+
+  function decrypt($ciphertext, $key)
+  {
+    $plaintext = "";
+    $length = strlen($ciphertext);
+    for ($i = 0; $i < $length; $i++) {
+      $char = ord($ciphertext[$i]);
+      if ($char >= 65 && $char <= 90) {
+        // uppercase letters
+        $newChar = (($char - 65 - $key + 26) % 26) + 65;
+      } else if ($char >= 97 && $char <= 122) {
+        // lowercase letters
+        $newChar = (($char - 97 - $key + 26) % 26) + 97;
+      } else {
+        // non-alphabetic characters
+        $newChar = $char;
       }
-      return $plaintext;
+      $plaintext .= chr($newChar);
     }
-    
-    if (isset($_POST['encrypt']) && isset($_POST['plaintext']) && isset($_POST['key'])) {
-      $plaintext = $_POST['plaintext'];
-      $key = $_POST['key'];
-      $ciphertext = encrypt($plaintext, $key);
-      echo "<p>Plain Text : " . $plaintext . "</p>";
-      echo "<p>Encrypted Text: " . $ciphertext . "</p>";
-    }
-    if (isset($_POST['decrypt']) && isset($_POST['cipher-text']) && isset($_POST['key'])) {
-      $ciphertext = $_POST['cipher-text'];
-      $key = $_POST['key'];
-      $plaintext = decrypt($ciphertext, $key);
-      echo "<p>Cipher Text : " . $ciphertext . "</p>";
-      echo "<p>Decrypted Text : " . $plaintext . "</p>";
-    }
+    return $plaintext;
+  }
   ?>
 </body>
+
 </html>
